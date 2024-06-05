e95500ab8abd637d03f9d5cdc50b3e59c5ad74aa
davadennis <davaovo@gmail.com>
2024-06-05 18:05:08 +0700

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,16 @@
+// dashboard.js
+import { auth, db } from './firebase-config.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            document.getElementById('welcome-message').innerText = `Halo, ${userData.username}`;
+        }
+    } else {
+        window.location.href = 'index.html'; // Redirect to login page if not logged in
+    }
+});
